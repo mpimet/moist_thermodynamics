283f3d54f71434ff783238b9859db20df06abea4
bjorn-stevens <64255981+bjorn-stevens@users.noreply.github.com>
2022-08-10 14:38:11 +0200

    [feature] modified calls to sat vapor pressure
    Initially saturation vapor pressure calls could select which formulation
    to use based on a string argument.  Now all calls are made explicitly
    visible and a default form is passed to functions that depend on it.
    Calls to these functions can however substitute a different form for the
    default form.

--- aes_thermo/functions.py
+++ aes_thermo/functions.py
@@ -3,11 +3,9 @@
 Author: Bjorn Stevens (bjorn.stevens@mpimet.mpg.de)
 """
 #
-import .constants
+import constants
 import numpy as np
 from scipy import interpolate, optimize
-
-es_default = 'analytic-liq'
 
 def planck(T,nu):
     """returns the Planck source function for the given temperature (K) and frequency (Hz)
@@ -19,69 +17,94 @@ def planck(T,nu):
 
     return (2 * h * nu**3 / c**2) / (np.exp(h*nu/(kB*T))-1)
 
-def saturation_vapor_pressure_water(T,es_formula=es_default):
+def saturation_vapor_pressure_water_liq(T):
     """ Returns the saturation vapor pressure of water over liquid or ice, or the minimum of the two,
     depending on the specificaiton of the state variable.  The calculation follows Wagner and Pruss (2002)
-    fits (es[li]f) for saturation over planar liquid, and Wagner et al., 2011 for saturation over ice.  The choice
-    of formulation was based on a comparision of many many formulae, among them those by Sonntag, Hardy,
-    Romps, Murphy and Koop, and others (e.g., Bolton) just over liquid. The Wagner and Pruss and Wagner
-    formulations were found to be the most accurate as compared to the IAPWS standard for warm temperatures,
-    and the Wagner et al 2011 form is the IAPWS standard for ice.  Additionally an 'analytic' expression es[li]a
-    for computations that require consisntency with assumption of cp's being constant can be selected.  The analytic
-    expressions become identical to Romps in the case when the specific heats are adjusted to his suggested values.
+    fits for saturation over planar liquid. The Wagner and Pruss formulations were found to be the most 
+    accurate as compared to the IAPWS standard for warm temperatures
+    """
+    TvC = constants.temperature_water_vapor_critical_point
+    PvC = constants.pressure_water_vapor_critical_point
+
+    vt = 1.-T/TvC
+    es = PvC * np.exp(TvC/T * (-7.85951783*vt + 1.84408259*vt**1.5 - 11.7866497*vt**3 + 22.6807411*vt**3.5 - 15.9618719*vt**4 + 1.80122502*vt**7.5))
+
+    return es
+
+def saturation_vapor_pressure_water_ice(T):
+    """ Returns the saturation vapor pressure of water over liquid or ice, or the minimum of the two,
+    depending on the specificaiton of the state variable.  The calculation follows Wagner et al., 2011 
+    fits for saturation over ice. The Wagner et al 2011 form is the IAPWS standard for ice.
+    """
+    TvT = constants.temperature_water_vapor_triple_point
+    PvT = constants.pressure_water_vapor_triple_point
+
+    a1 = -0.212144006e+2
+    a2 =  0.273203819e+2
+    a3 = -0.610598130e+1
+    b1 =  0.333333333e-2
+    b2 =  0.120666667e+1
+    b3 =  0.170333333e+1
+    theta = T/TvT
+    es =  PvT * np.exp((a1*theta**b1 + a2 * theta**b2 + a3 * theta**b3)/theta)
+
+    return es
+
+def saturation_vapor_pressure_water_mxd(T):
+    """ Returns the minimum of the saturation vapor pressure of water over liquid or ice, which should
+    default to the ice value at temperatures below the freezing point and the liquid value otherwise
+    """
+    esl = saturation_vapor_pressure_water_liq
+    esi = saturation_vapor_pressure_water_ice
+
+    return np.minimum(esl(x),esi(x))
+
+def analytic_saturation_vapor_pressure_water_liq(T):
+    """ Returns an analytic approximation to the saturation vapor pressure over liquid.  Useful for
+    computations that require consisntency with assumption of cp's being constant.  The analytic expressions
+    become identical to Romps in the case when the specific heats are adjusted to his suggested values.
     >>> es([273.16,290.])
     [611.65706974 1919.87719485]
     """
     TvT = constants.temperature_water_vapor_triple_point
     PvT = constants.pressure_water_vapor_triple_point
-    TvC = constants.temperature_water_vapor_critical_point
-    PvC = constants.pressure_water_vapor_critical_point
     lvT = constants.vaporization_enthalpy_triple_point
-    lsT = constants.sublimation_enthalpy_triple_point
     Rv  = constants.water_vapor_gas_constant
     cpv = constants.isobaric_water_vapor_specific_heat
     cl  = constants.liquid_water_specific_heat
 
-    def esif(T):
-        a1 = -0.212144006e+2
-        a2 =  0.273203819e+2
-        a3 = -0.610598130e+1
-        b1 =  0.333333333e-2
-        b2 =  0.120666667e+1
-        b3 =  0.170333333e+1
-        theta = T/TvT
-        return PvT * np.exp((a1*theta**b1 + a2 * theta**b2 + a3 * theta**b3)/theta)
-
-    def eslf(T):
-        vt = 1.-T/TvC
-        return PvC * np.exp(TvC/T * (-7.85951783*vt + 1.84408259*vt**1.5 - 11.7866497*vt**3 + 22.6807411*vt**3.5 - 15.9618719*vt**4 + 1.80122502*vt**7.5))
-
-    def esla(T):
-        c1 = (cpv-cl)/Rv
-        c2 = lvT/(Rv*TvT) - c1
-        return PvT * np.exp(c2*(1.-TvT/T)) * (T/TvT)**c1
-
-    def esia(T):
-        c1 = (cpv-ci)/Rv
-        c2 = lsT/(Rv*TvT) - c1
-        return PvT * np.exp(c2*(1.-TvT/T)) * (T/TvT)**c1
-
-    x = T
-
-    if (es_formula == 'liq'):
-        es = eslf(x)
-    if (es_formula == 'ice'):
-        es = esif(x)
-    if (es_formula == 'mxd'):
-        es = np.minimum(esif(x),eslf(x))
-    if (es_formula == 'analytic-liq'):
-        es = esla(x)
-    if (es_formula == 'analytic-ice'):
-        es = esia(x)
-    if (es_formula == 'analytic-mxd'):
-        es = np.minimum(esia(x),esla(x))
-
+    c1 = (cpv-cl)/Rv
+    c2 = lvT/(Rv*TvT) - c1
+    es = PvT * np.exp(c2*(1.-TvT/T)) * (T/TvT)**c1
     return es
+
+def analytic_saturation_vapor_pressure_water_ice(T):
+    """ Returns an analytic approximation to the saturation vapor pressure over ice.  Useful for
+    computations that require consisntency with assumption of cp's being constant.  The analytic expressions
+    become identical to Romps in the case when the specific heats are adjusted to his suggested values.
+    >>> es([273.16,290.])
+    [611.65706974 1919.87719485]
+    """
+    TvT = constants.temperature_water_vapor_triple_point
+    PvT = constants.pressure_water_vapor_triple_point
+    lsT = constants.sublimation_enthalpy_triple_point
+    Rv  = constants.water_vapor_gas_constant
+    cpv = constants.isobaric_water_vapor_specific_heat
+    ci  = constants.frozen_water_specific_heat
+
+    c1 = (cpv-ci)/Rv
+    c2 = lsT/(Rv*TvT) - c1
+    es = PvT * np.exp(c2*(1.-TvT/T)) * (T/TvT)**c1
+    return es
+
+def analytic_saturation_vapor_pressure_water_mxd(T):
+    """ Returns the minimum of the saturation vapor pressure of water over liquid or ice, which should
+    default to the ice value at temperatures below the freezing point and the liquid value otherwise
+    """
+    esl = analytic_saturation_vapor_pressure_water_liq
+    esi = analytic_saturation_vapor_pressure_water_ice
+
+    return np.minimum(esl(x),esi(x))
 
 def phase_change_enthalpy(TK,fusion=False):
     """ Returns the enthlapy [J/g] of vaporization (default) of water vapor or
@@ -96,8 +119,6 @@ def phase_change_enthalpy(TK,fusion=False):
     cpv = constants.isobaric_water_vapor_specific_heat
     cl  = constants.liquid_water_specific_heat
     ci  = constants.frozen_water_specific_heat
-
-
 
     if (fusion):
         el = lf0 + (cl-ci)*(TK-T0)
@@ -135,17 +156,16 @@ def mixing_ratio_to_partial_pressure(r,p):
     eps1 = constants.rd_over_rv
     return r*p/(eps1+r)
 
-def pseudo_theta_e(TK,PPa,qt,es_formula=es_default):
+def pseudo_theta_e(TK,PPa,qt,es=saturation_vapor_pressure_water_liq):
     """ Calculates pseudo equivalent potential temperature. following Bolton
     checked 31.07.20
     """
 
     P0    = constants.standard_pressure
-    es    = saturation_vapor_pressure_water
     p2r   = partial_pressure_to_mixing_ratio
     r2p   = mixing_ratio_to_partial_pressure
     
-    rv = np.minimum(qt/(1.-qt), p2r(es(TK,es_formula),PPa))  # mixing ratio of vapor (not gas Rv)
+    rv = np.minimum(qt/(1.-qt), p2r(es(TK),PPa))  # mixing ratio of vapor (not gas Rv)
     pv = r2p(rv,PPa)
 
     TL     = 55.0 + 2840./(3.5*np.log(TK) - np.log(pv/100.) - 4.805)
@@ -153,7 +173,7 @@ def pseudo_theta_e(TK,PPa,qt,es_formula=es_default):
     
     return(theta_e)
 
-def theta_e(TK,PPa,qt,es_formula=es_default):
+def theta_e(TK,PPa,qt,es=saturation_vapor_pressure_water_liq):
     """ Calculates equivalent potential temperature corresponding to Eq. 2.42 in the Clouds
     and Climate book.
     """
@@ -161,11 +181,10 @@ def theta_e(TK,PPa,qt,es_formula=es_default):
     P0   = constants.standard_pressure
     Rd   = constants.dry_air_gas_constant
     Rv   = constants.water_vapor_gas_constant
-    es   = saturation_vapor_pressure_water
     lv   = phase_change_enthalpy
     p2r  = partial_pressure_to_mixing_ratio
 
-    ps = es(TK,es_formula)
+    ps = es(TK)
     qs = p2r(ps,PPa) * (1.0 - qt)
     qv = np.minimum(qt,qs)
 
@@ -179,7 +198,7 @@ def theta_e(TK,PPa,qt,es_formula=es_default):
 
     return(theta_e)
 
-def theta_es(TK,PPa,es_formula=es_default):
+def theta_es(TK,PPa,es=saturation_vapor_pressure_water_liq):
     """ Calculates equivalent potential temperature corresponding to Eq. 2.42 in the Clouds
     and Climate book but assuming that the water amount is just saturated
     """
@@ -191,7 +210,7 @@ def theta_es(TK,PPa,es_formula=es_default):
     cpv  = constants.isobaric_water_vapor_specific_heat
     p2q  = partial_pressure_to_specific_humidity
 
-    ps = es(TK,es_formula)
+    ps = es(TK)
     qs = p2q(ps,PPa)
 
     Re = (1.0-qs)*Rd
@@ -203,7 +222,7 @@ def theta_es(TK,PPa,es_formula=es_default):
     return(theta_es)
 
 
-def theta_l(TK,PPa,qt,es_formula=es_default):
+def theta_l(TK,PPa,qt,es=saturation_vapor_pressure_water_liq):
     """ Calculates liquid-water potential temperature.  Following Stevens and Siebesma
     Eq. 2.44-2.45 in the Clouds and Climate book
     """
@@ -215,10 +234,9 @@ def theta_l(TK,PPa,qt,es_formula=es_default):
     cpv  = constants.isobaric_water_vapor_specific_heat
     eps1 = constants.rd_over_rv
 
-    es   = saturation_vapor_pressure_water
     lv   = phase_change_enthalpy
 
-    ps = es(TK,es_formula)
+    ps = es(TK)
     qs = (ps/(PPa-ps)) * eps1 * (1. - qt)
     qv = np.minimum(qt,qs)
     ql = qt-qv
@@ -232,7 +250,7 @@ def theta_l(TK,PPa,qt,es_formula=es_default):
 
     return(theta_l)
 
-def theta_s(TK,PPa,qt,es_formula=es_default):
+def theta_s(TK,PPa,qt,es=saturation_vapor_pressure_water_liq):
     """ Calculates entropy potential temperature. This follows the formulation of Pascal
     Marquet and ensures that parcels with different theta-s have a different entropy
     """
@@ -247,11 +265,10 @@ def theta_s(TK,PPa,qt,es_formula=es_default):
     eps2 = constants.rv_over_rd_minus_one
     
     p2r  = partial_pressure_to_mixing_ratio
-    es   = saturation_vapor_pressure_water
     lv   = phase_change_enthalpy
 
     kappa = Rd/cpd
-    e0    = es(T0,es_formula)
+    e0    = es(T0)
     Lmbd  = ((sv00 - Rv*np.log(e0/P0)) - (sd00 - Rd*np.log(1-e0/P0)))/cpd
     lmbd  = cpv/cpd - 1.
     eta   = 1/eps1
@@ -259,7 +276,7 @@ def theta_s(TK,PPa,qt,es_formula=es_default):
     gamma = kappa/eps1
     r0    = e0/(P0-e0)/eta
 
-    ps = es(TK,es_formula)
+    ps = es(TK)
     qs = p2r(ps,PPa) * (1. - qt)
     qv = np.minimum(qt,qs)
     ql = qt-qv
@@ -276,25 +293,23 @@ def theta_s(TK,PPa,qt,es_formula=es_default):
 
     return(theta_s)
 
-def theta_rho(TK,PPa,qt,es_formula=es_default):
+def theta_rho(TK,PPa,qt,es=saturation_vapor_pressure_water_liq):
     """ Calculates theta_rho as theta_l * (1+Rd/Rv qv - qt)
     """
 
     Rd   = constants.dry_air_gas_constant
     Rv   = constants.water_vapor_gas_constant
     eps1 = constants.rd_over_rv
-    es   = saturation_vapor_pressure_water
     p2r  = partial_pressure_to_mixing_ratio
 
-
-    ps = es(TK,es_formula)
+    ps = es(TK)
     qs = p2r(ps,PPa) * (1. - qt)
     qv = np.minimum(qt,qs)
-    theta_rho = theta_l(TK,PPa,qt,es_formula) * (1.+ qv/eps1 - qt)
+    theta_rho = theta_l(TK,PPa,qt,es) * (1.+ qv/eps1 - qt)
 
     return(theta_rho)
 
-def T_from_Te(Te,P,qt,es_formula=es_default):
+def T_from_Te(Te,P,qt,es=saturation_vapor_pressure_water_liq):
     """ Given theta_e solves implicitly for the temperature at some other pressure,
     so that theta_e(T,P,qt) = Te
 	>>> T_from_Te(350.,1000.,17)
@@ -303,11 +318,11 @@ def T_from_Te(Te,P,qt,es_formula=es_default):
 
     def zero(T,Te,P,qt):
         _theta_e = np.vectorize(theta_e)
-        return  np.abs(Te-_theta_e(T,P,qt,es_formula))
+        return  np.abs(Te-_theta_e(T,P,qt,es=es))
     
     return optimize.fsolve(zero,   280., args=(Te,P,qt), xtol=1.e-10)
 
-def T_from_Tl(Tl,P,qt,es_formula=es_default):
+def T_from_Tl(Tl,P,qt,es=saturation_vapor_pressure_water_liq):
     """ Given theta_e solves implicitly for the temperature at some other pressure,
     so that theta_e(T,P,qt) = Te
 	>>> T_from_Tl(282.75436951,90000,20.e-3)
@@ -315,11 +330,11 @@ def T_from_Tl(Tl,P,qt,es_formula=es_default):
     """
     def zero(T,Tl,P,qt):
         _theta_l = np.vectorize(theta_l)
-        return  np.abs(Tl-_theta_l(T,P,qt,es_formula))
+        return  np.abs(Tl-_theta_l(T,P,qt,es=es))
     
     return optimize.fsolve(zero,   280., args=(Tl,P,qt), xtol=1.e-10)
 
-def T_from_Ts(Ts,P,qt,es_formula=es_default):
+def T_from_Ts(Ts,P,qt,es=saturation_vapor_pressure_water_liq):
     """ Given theta_e solves implicitly for the temperature at some other pressure,
     so that theta_e(T,P,qt) = Te
 	>>> T_from_Tl(282.75436951,90000,20.e-3)
@@ -327,11 +342,11 @@ def T_from_Ts(Ts,P,qt,es_formula=es_default):
     """
     def zero(T,Ts,P,qt):
         _theta_s = np.vectorize(theta_s)
-        return  np.abs(Ts-_theta_s(T,P,qt,es_formula))
+        return  np.abs(Ts-_theta_s(T,P,qt,es=es))
     
     return optimize.fsolve(zero,   280., args=(Ts,P,qt), xtol=1.e-10)
 
-def P_from_Te(Te,T,qt,es_formula=es_default):
+def P_from_Te(Te,T,qt,es=saturation_vapor_pressure_water_liq):
     """ Given Te solves implicitly for the pressure at some temperature and qt
     so that theta_e(T,P,qt) = Te
 	>>> P_from_Te(350.,305.,17)
@@ -339,10 +354,10 @@ def P_from_Te(Te,T,qt,es_formula=es_default):
     """
     def zero(P,Te,T,qt):
         _theta_e = np.vectorize(theta_e)
-        return np.abs(Te-_theta_e(T,P,qt,es_formula))
+        return np.abs(Te-_theta_e(T,P,qt,es=es))
     return optimize.fsolve(zero, 90000., args=(Te,T,qt), xtol=1.e-10)
 
-def P_from_Tl(Tl,T,qt,es_formula=es_default):
+def P_from_Tl(Tl,T,qt,es=saturation_vapor_pressure_water_liq):
     """ Given Tl solves implicitly for the pressure at some temperature and qt
     so that theta_l(T,P,qt) = Tl
 	>>> T_from_Tl(282.75436951,290,20.e-3)
@@ -350,11 +365,11 @@ def P_from_Tl(Tl,T,qt,es_formula=es_default):
     """
     def zero(P,Tl,T,qt):
         _theta_l = np.vectorize(theta_l)
-        return np.abs(Tl-_theta_l(T,P,qt,es_formula))
+        return np.abs(Tl-_theta_l(T,P,qt,es=es))
     
     return optimize.fsolve(zero, 90000., args=(Tl,T,qt), xtol=1.e-10)
 
-def Plcl(TK,PPa,qt,es_formula=es_default,iterate=False):
+def Plcl(TK,PPa,qt,es=saturation_vapor_pressure_water_liq,iterate=False):
     """ Returns the pressure [Pa] of the LCL.  The routine gives as a default the
     LCL using the Bolton formula.  If iterate is true uses a nested optimization to
     estimate at what pressure, Px and temperature, Tx, qt = qs(Tx,Px), subject to
@@ -368,18 +383,16 @@ def Plcl(TK,PPa,qt,es_formula=es_default,iterate=False):
     cpd  = constants.isobaric_dry_air_specific_heat
     cpv  = constants.isobaric_water_vapor_specific_heat
     eps1 = constants.rd_over_rv
-    es   = saturation_vapor_pressure_water
     r2p  = mixing_ratio_to_partial_pressure
 
-
-    def delta_qs(P,Te,qt,es_formula=es_default):
+    def delta_qs(P,Te,qt,es=saturation_vapor_pressure_water_liq):
         TK = T_from_Te(Te,P,qt)
-        ps = es(TK,es_formula)
+        ps = es(TK)
         qs = (1./(P/ps-1.)) * eps1 * (1. - qt)
         return np.abs(qs/qt-1.)
 
     if (iterate):
-        Te   = theta_e(TK,PPa,qt,es_formula)
+        Te   = theta_e(TK,PPa,qt,es)
         zero = np.vectorize(delta_qs)
         if scalar_input:
             Plcl = optimize.fsolve(zero, 80000., args=(Te,qt), xtol=1.e-10)
